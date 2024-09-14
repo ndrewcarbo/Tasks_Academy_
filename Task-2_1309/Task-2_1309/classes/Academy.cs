2e341eda7929c0357de4d8cf12cf0303820874a7
Andrea Carbone <andrea.carbone0612@gmail.com>
2024-09-14 17:12:47 +0200

    prova

---
+++ Task-2_1309/Task-2_1309/classes/Academy.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_2_1309.classes
+{
+    internal class Academy
+    {
+        private static int contatore_Iscrizioni = 0;
+        public string? Nome { get; set; }
+        public string? Indirizzo { get; set; }
+
+        public List<Studente>? Elenco { get; set; } = null;
+
+
+
+
+        public void Settembre(Studente stud)
+        {
+            if (Elenco is null)
+            {
+                 Elenco = new List<Studente>();
+            }
+            else
+            {
+                Elenco.Add(stud);
+            }
+        }
+
+
+        public void Iscrizione(Studente stud)
+        {
+            Elenco?.Add(stud);
+        }
+    }
+}

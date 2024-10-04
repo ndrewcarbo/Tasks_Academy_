437e98e05ede5f426c16e3a2012490c1c550ee2e
Andrea Carbone <andrea.carbone0612@gmail.com>
2024-10-04 18:56:58 +0200

    Harrypotter

---
+++ Task_HPotter/script.js
@@ -0,0 +1,96 @@
+// let elencoCasate = localStorage.getItem("Hogwarts");
+// let elenco = [];
+
+// //localstorage inizializzato
+
+// if(elencoCasate == null){
+//     localStorage.setItem("Hogwarts",JSON.stringify(elenco))
+// } else {
+//     elenco=JSON.parse(elencoCasate)
+// }
+
+
+
+// Ogni casata è caratterizzata da 
+//     -nome
+//     -descizione
+//     -LOGO
+//     -numero di bacchette
+
+function aggiungi(){
+    let local = localStorage.getItem("Hogwarts") != null ? JSON.parse(localStorage.getItem("Hogwarts")) : []; //controllo se è nullo altrimenti è vuoto
+
+    // let idx = document.getElementById("id-idx").value
+    let varNome = document.getElementById("id-nome").value
+    let varDesc = document.getElementById("id-desc").value
+    let varLogo = document.getElementById("id-logo").value
+    let varBac = document.getElementById("id-num-bac").value
+
+    //CREO NUOVA CASATA
+
+    let newCas = {
+        nome: varNome,
+        descrizione: varDesc,
+        logo: varLogo,
+        bacchette: varBac
+    }
+    //PUSH
+   
+    local.push(newCas);
+    localStorage.setItem("Hogwarts", JSON.stringify(local))
+    StampaCasate();
+}
+
+
+function StampaCasate (){
+    let local = localStorage.getItem("Hogwarts") != null ? JSON.parse(localStorage.getItem("Hogwarts")) : []; //controllo se è nullo altrimenti è vuoto
+
+    let contenitore = "";
+
+    for(let [index, item] of local.entries()){
+        contenitore +=
+         `
+        <tr>
+            <td>${index + 1}</td>
+            <td>${item.nome}</td>
+            <td>${item.descrizione}</td>
+            <td>${item.logo}</td>
+            <td>${item.bacchette}</td>
+        </tr>
+        
+        `
+    };
+    
+    document.getElementById("corpo-tabella-casata").innerHTML= contenitore
+}
+StampaCasate();
+
+
+function DettaglioCasata(){
+    imgs = ["img/grifondoro.PNG","img/serpeverde.PNG","img/corvonero.PNG","img/tassorosso.PNG"];
+
+
+
+}
+
+
+// function DettaglioCasata(varBottone){
+
+    
+//     let varNome = document.getElementById("input-nome").value;
+//     let varLogo = document.getElementById("input-cognome").value;
+//     let varDesc = document.getElementById("input-matricola").value;
+//     let posizione = $(varBottone).data('identif');
+//     let local = localStorage.getItem("Hogwarts") != null ? JSON.parse(localStorage.getItem("Hogwarts")) : []; //controllo se è nullo altrimenti è vuoto
+//     let casa = ""
+//     for(let [index,item] of local.entries()){
+//         if(index == posizione){
+//             item.nome = varNome;
+//             item.logo = varLogo;
+//             item.descrizione = varDesc;
+
+//         }
+//     }
+
+
+// }
---
+++ Task_HPotter/shop.js
@@ -0,0 +1,103 @@
+// let elencoBacchette = localStorage.getItem("SHOP");
+// let oli = [];
+
+// if(elencoBacchette==null){
+//     localStorage.setItem("SHOP",JSON.stringify(oli))
+// }
+// else {
+//     oli=JSON.parse(elencoBacchette)
+// }
+
+
+
+function elimina(index){
+    let oli = localStorage.getItem("SHOP") != null ? JSON.parse(localStorage.getItem("SHOP")) : [];
+
+    oli.splice(index,1);
+
+    localStorage.setItem("SHOP",JSON.stringify(oli))
+
+    StampaBac()
+
+}
+
+function addBacc (){
+   let oli = localStorage.getItem("SHOP") != null ? JSON.parse(localStorage.getItem("SHOP")) : [];
+
+   let varCode = document.getElementById("input-cod").value
+   let varMat = document.getElementById("input-mate").value
+   let varNuc = document.getElementById("input-nucleo").value
+   let varLun = document.getElementById("input-lunghezza").value
+   let varCas = document.getElementById("input-casata").value
+   let varFoto = document.getElementById("input-foto").value
+
+
+
+   let newBac = {
+    codice: varCode,
+    materiale: varMat,
+    nucleo: varNuc,
+    lunghezza: varLun,
+    casata: varCas,
+    foto: varFoto
+}
+
+
+oli.push(newBac);
+    localStorage.setItem("SHOP",JSON.stringify(oli));
+
+
+
+}
+
+
+function StampaBac (){
+    let oli = localStorage.getItem("SHOP") != null ? JSON.parse(localStorage.getItem("SHOP")) : []; //controllo se è nullo altrimenti è vuoto
+
+    let elebac = "";
+
+    for(let [index, item] of oli.entries()){
+        elebac += `
+        <tr>
+            <td>${index + 1}</td>
+            <td>${item.codice}</td>
+            <td>${item.materiale}</td>
+            <td>${item.nucleo}</td>
+            <td>${item.lunghezza}</td>
+            <td>${item.casata}</td>
+            <td>${item.foto}</td>
+
+            <td>
+                    <button type="button" class="btn btn-danger" onclick="elimina(${index})">Elimina</button>
+                    <button type="button" class="btn btn-warning" onclick="modifica(${index})">Modifica</button>
+            </td>
+        </tr>
+
+
+                
+        `;
+
+
+    }
+
+    document.getElementById("corpo-tabella-shop").innerHTML= elebac;
+}
+
+
+StampaBac();
+
+function modifica(indice){
+    $("#modaleModifica").modal('show');
+    $("#btn-salva").data('identif', indice);
+
+    let elencoLocale = localStorage.getItem("scuola") != null 
+                            ? JSON.parse(localStorage.getItem("scuola")) : [];
+
+    for(let [index, item] of elencoLocale.entries()){
+        if(indice == index){
+            $("#input-nome").val(item.nome);                                //Notazione JQuery
+            document.getElementById("input-casata").value = item.casata;
+            
+        }
+    }
+}
